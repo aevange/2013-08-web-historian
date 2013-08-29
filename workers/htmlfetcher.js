7e88be8f0106dd6695ab0cac3478258cbddccc6d
Hack Reactor Students <github@hackreactor.com>
2013-08-29 12:26:24 -0700

    All specs pass, sucessfully fetches urls, cron automates every minute.

--- workers/htmlfetcher.js
+++ workers/htmlfetcher.js
@@ -1,2 +1,14 @@
-// eventually, you'll have some code here that uses the tested helpers 
+// eventually, you'll have some code here that uses the tested helpers
 // to actually download the urls you want to download.
+var fs = require("fs");
+var url = require("url");
+var path = require("path");
+var helpers = require("./lib/html-fetcher-helpers");
+
+var filePath = path.join(path.dirname(__dirname), "/data/sites.txt");
+
+
+helpers.readUrls(filePath, helpers.downloadUrls);
+
+
+//* * * * * /Users/hackreactor/.nvm/v0.10.16/bin/node /Users/hackreactor/code/aevange/2013-08-web-historian/workers/htmlfetcher.js
--- workers/lib/html-fetcher-helpers.js
+++ workers/lib/html-fetcher-helpers.js
@@ -1,7 +1,47 @@
-exports.readUrls = function(filePath, cb){
+var fs = require("fs");
+var url = require("url");
+var path = require("path");
+var http = require("http");
 
+var filePath = path.dirname(path.dirname(__dirname)) + "/data/sites/";
+
+
+exports.readUrls = function(filePath, cb){
+  fs.readFile(filePath, 'utf8', function(err, buffer){
+    var data = buffer.split('\n');
+    cb(data);
+  });
 };
 
 exports.downloadUrls = function(urls){
-  // fixme
+  for (var i = 0; i < urls.length; i++) {
+    downloadUrl(urls[i]);
+  }
+};
+
+var downloadUrl = function(url){
+  var options = {
+    host: url,
+    port: 80,
+    method: 'GET'
+  };
+
+  http.get(options, function(res){
+    var fullHTML = "";
+
+    res.on('data', function(chunk){
+      res.setEncoding('utf8');
+      fullHTML += chunk;
+    });
+
+    res.on('end', function(){
+      //now want to write "BODY" to file in sites with the file name matching the url
+      //wite to file
+      fs.writeFile(filePath + url, fullHTML, function(err){
+        err && console.log(err);
+      });
+
+      console.log("done!");
+    });
+  });
 };
