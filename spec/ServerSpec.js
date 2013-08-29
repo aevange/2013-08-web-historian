a82748a1c0f32d053fbe5b59c26cae6a12674bcd
Hack Reactor Students <github@hackreactor.com>
2013-08-28 20:04:42 -0700

    Passes all ServerSpect tests

--- spec/ServerSpec.js
+++ spec/ServerSpec.js
@@ -1,5 +1,6 @@
+var fs  = require("fs");
 var handler = require("../web/request-handler");
-handler.datadir = __dirname + "testdata/sites.txt";
+handler.datadir = __dirname + "/testdata/sites.txt";
 var stubs = require("./helpers/stubs");
 var res;
 
@@ -33,7 +34,7 @@ describe("Node Server Request Listener Function", function() {
       expect(res._responseCode).toEqual(200);
       expect(res._data).toMatch(/google/); // the resulting html should have the text "google"
       expect(res._ended).toEqual(true);
-    })
+    });
   });
 
   it("Should accept posts to /", function() {
@@ -44,10 +45,12 @@ describe("Node Server Request Listener Function", function() {
 
     handler.handleRequest(req, res);
 
-    var fileContents = fs.readFileSync(handler.datadir, 'utf8');
-    expect(res._responseCode).toEqual(302);
-    expect(fileContents).toEqual(url + "\n");
-    expect(res._ended).toEqual(true);
+    async(function(){
+      var fileContents = fs.readFileSync(handler.datadir, 'utf8');
+      expect(res._responseCode).toEqual(302);
+      expect(fileContents).toEqual(url + "\n");
+      expect(res._ended).toEqual(true);
+    });
   });
 
   it("Should 404 when asked for a nonexistent file", function() {
--- web/request-handler.js
+++ web/request-handler.js
@@ -1,6 +1,72 @@
+var fs   = require('fs');
+var http = require("http");
+var url  = require("url");
 var path = require('path');
+
+var headers = {
+  "access-control-allow-origin": "*",
+  "access-control-allow-methods": "GET, POST, PUT, DELETE, OPTIONS",
+  "access-control-allow-headers": "content-type, accept",
+  "access-control-max-age": 10, // Seconds.
+  "Content-Type": "text/plain"
+};
+
+
+var handlePOST = function(req, res){
+  var fullFile = "";
+
+  req.on("data", function(chunk){
+    fullFile += chunk;
+  });
+  req.on("end", function(){
+    var urlStuff = fullFile.split("=");
+
+    fs.appendFile(module.exports.datadir, urlStuff[1] + "\n",function(err){
+
+      if(err){
+        res.writeHead(404,headers);
+        res.end();
+      } else {
+        res.writeHead(302,headers);
+        res.end();
+      }
+    });
+  });
+};
+
+var handleGET = function(req,res){
+  var pathname = url.parse(req.url,true,true).pathname;
+  var toRead;
+  if(pathname === '/'){
+    toRead = __dirname + "/public/index.html";
+  } else {
+    toRead = path.dirname(__dirname) + "/data/sites" + pathname;
+  }
+
+  fs.readFile(toRead, function(err, data){
+    if(err){
+      res.writeHead(404,headers);
+      res.end();
+    } else {
+      res.writeHead(200,headers);
+      res.end(data);
+    }
+  });
+};
+
 module.exports.datadir = path.join(__dirname, "../data/sites.txt"); // tests will need to override this.
 
 module.exports.handleRequest = function (req, res) {
+  var urlDiscover = url.parse(req.url,true,true);
+  //if get request run this
+  if(req.method === 'GET'){
+    handleGET(req,res);
+  } else if (req.method === 'POST'){
+    handlePOST(req,res);
+  } else {
+    res.writeHead(404,headers);
+    res.end();
+  }
+
   console.log(exports.datadir);
 };
